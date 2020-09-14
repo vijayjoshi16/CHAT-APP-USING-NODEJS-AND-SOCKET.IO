489e3ba568cc176dfb7a3b989c86eb38cc5201c3
vijayjoshi16 <joshivijay324@gmail.com>
2020-09-14 19:52:29 +0530

    Second commit

---
+++ node/index.js
@@ -0,0 +1,19 @@
+//SOCKET IO CONNECTIONS WILL BE HANDLED BY THIS NODE SERVER
+const io=require("socket.io")(8000);
+
+const users={};
+
+io.on("connection",socket=>{
+    socket.on('new-user-joined',name=>{
+        console.log("New User",name);
+        users[socket.id]=name;
+        socket.broadcast.emit('user-joined',name);
+    });
+    socket.on('send',message=>{
+        socket.broadcast.emit('receive',{message:message,name:users[socket.id]});
+    });
+    socket.on('disconnect',message=>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    });
+});
